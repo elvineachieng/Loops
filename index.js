5a629d9d02d04cea9c775c8d1406a744d0e4b614
Elvine7067 <achiengelvine1@gmail.com>
2023-07-11 14:29:53 +0300

    Loops

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  // code that gets executed as long as the condition is true
 } 
 */
-let i = 0;//creates a variable i and  sets it to 0
+let i = 1;//creates a variable i and  sets it to 0
 while(i<10){//start the while loop and check the condition that the value of i is smaller than 10
     console.log(i);//the condition is true thus the code logs i and increases by 1 
     i++;
