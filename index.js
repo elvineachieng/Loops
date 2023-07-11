c93c6da9396e5c4639bab4ce4f0a363976ab0d72
Elvine7067 <achiengelvine1@gmail.com>
2023-07-11 14:26:43 +0300

    Loops in JS

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 //A while loop executes a certain block of code as long as an expression evaluates to true
 /* while (condition) {
  // code that gets executed as long as the condition is true
-} */
+} 
+*/
 let i = 0;//creates a variable i and  sets it to 0
 while(i<10){//start the while loop and check the condition that the value of i is smaller than 10
     console.log(i);//the condition is true thus the code logs i and increases by 1 
