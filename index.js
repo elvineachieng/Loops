f608d416c609416d230e7d473d3a3eeae7e92ea1
Elvine7067 <achiengelvine1@gmail.com>
2023-07-13 08:56:18 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+/*while (condition) {
+ // code that gets executed as long as the condition is true
+}*/
 let i = 0;
 while (i < 10) {
  console.log(i);
  i++;
+}
+let someArray = ["Mike", "Antal", "Marc", "Emir", "Louiza", "Jacky"];
+let notFound = true;
+while (notFound && someArray.length > 0) {
+ if (someArray[0] === "Louiza") {
+ console.log("Found her!");
+ notFound = false;
+ } else {
+ someArray.shift();
+ }
 }
